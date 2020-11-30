674aa9f4311f6f0bc9eea018770cb50a39492b9f
Maaz Khan <59374050+MaazKhan01@users.noreply.github.com>
2020-11-30 21:49:33 +0500

    Update Menu.java
    added some methods in class "Menu"

--- Menu.java
+++ Menu.java
@@ -1,29 +1,48 @@
 
-// Main class
 class Menu{
+    String name;
+    String isOpen;
+    
+    public void open(){
+        System.out.println("open from class Menu");
+    }
+    public void select(){
+        System.out.println("select from class Menu");
+    }
+    public void move(){
+        System.out.println("move method from class menu");
+    }
+    public void rotate(){
+        System.out.println("rotate method from class menu");
+    }
     public static void main (String[] args) {
-        
         //Object for Class Line
        Line ln = new Line();
        ln.draw();
        //Object for Circle class
        Circle crcl = new Circle();
        crcl.draw();
-       //Object for Triangle class
-       Triangle tri = new Triangle();
-       tri.draw();
-        
-       // initializing classes of Shapes and view class in class "Group" 
-       //display screen is always constant in a graphic aditor, there are varity of diffrent shapes we can make, Here we are just taking three(line, circle, triangle) Examples;
-       Group grp = new Group();
+        //class group
+        Group grp = new Group();
        grp.draw();
        grp.select();
-       grp.move();
-       grp.rotate();
-       grp.remove();
+        grp.remove();
        grp.group();
        grp.add();
        grp.show();
-     
+       grp.move();
+       grp.rotate();
+       //Object for Triangle class
+       Triangle tri = new Triangle();
+       tri.draw();
+      
+    
+       
+       
+       Main menu = new Main();
+       menu.open();
+       menu.select();
+       menu.move();
+       menu.rotate();
    }
 }
